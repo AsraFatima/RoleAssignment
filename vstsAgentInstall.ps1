14bb534a94474b9a2c853d266c859b761f3e7177
AsraFatima <asra-fatima@outlook.com>
2018-03-15 15:36:32 -0700

    Add files via upload

---
+++ vstsAgentInstall.ps1
@@ -0,0 +1,9 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+md \agent;
+cd \agent;
+$cwd = Get-Location;
+md \vstsagent.zip
+
+$destinationFolder = $cwd+"\vstsagent.zip"
+Invoke-WebRequest "https://github.com/Microsoft/vsts-agent/releases/download/v2.124.0/vsts-agent-win7-x64-2.124.0.zip" -OutFile "\vstsagent.zip" -UseBasicParsing
+Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory($agent, "$cwd")
