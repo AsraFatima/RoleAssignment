77d110d72c35b87830894433001c51fd7cba010c
AsraFatima <asra-fatima@outlook.com>
2018-03-20 13:51:38 -0700

    Update vstsAgentInstall.ps1

--- vstsAgentInstall.ps1
+++ vstsAgentInstall.ps1
@@ -1,10 +1,12 @@
 # Downloads the Visual Studio Online Build Agent, installs on the new machine, registers with the Visual
 # Studio Online account, and adds to the specified build agent pool
+# Downloads the Visual Studio Online Build Agent, installs on the new machine, registers with the Visual
+# Studio Online account, and adds to the specified build agent pool
 [CmdletBinding()]
 param(
     [string] $VstsAccount,
     [string] $VstsUserPassword,
-    [string] $PoolName,
+    [string] $PoolName
 )
 
 ###################################################################################################
@@ -52,6 +54,7 @@ function New-AgentInstallPath
         # Create the directory for this agent.
         $agentInstallPath = Join-Path -Path $agentInstallDir -ChildPath "\vstsAgent.zip"
         New-Item -ItemType Directory -Force -Path $agentInstallPath | Out-Null
+        Write-Host
         Invoke-WebRequest $agentUrl -OutFile "\vstsagent.zip" -UseBasicParsing
         Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory("\vstsagent.zip", $agentDir)
     }
@@ -89,9 +92,15 @@ function Install-Agent
 
     try
     {
+        cd \agent
+        $currenDir = Get-location
+        $agentInstallerDir = "VSTSAgent"
+        $agentInstallerPath - Join-path -Path $currenDir -ChildPath $agentInstallDir
+         Write-Host 'Set the current directory to the agent dedicated one previously created.'
         # Set the current directory to the agent dedicated one previously created.
-        pushd -Path $Config.AgentInstallPath
+        pushd -Path $agentInstallerPath
 
+        Write-Host 'Create a parameter for the Config.cmd'
         # The actual install of the agent. Using --runasservice, and some other values that could be turned into paramenters if needed.
         $agentConfigArgs = "--unattended", "--url", $Config.ServerUrl, "--auth", "PAT","--token", $Config.VstsUserPassword, "--pool", $Config.PoolName, "--runasservice"        
         .\config.cmd $agentConfigArgs
@@ -136,8 +145,7 @@ try
     # without prompting the user or blocking the cmd execution.
     Write-Host 'Installing agent'
     $config = @{
-       AgentExePath = $agentExePath
-       AgentInstallPath = $agentInstallPath        
+       AgentExePath = $agentExePath          
        PoolName = $PoolName
        ServerUrl = "https://$VstsAccount.visualstudio.com"
        VstsUserPassword = $VstsUserPassword 
